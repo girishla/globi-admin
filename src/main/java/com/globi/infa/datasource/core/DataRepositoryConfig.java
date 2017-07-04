11123483a51ddfab562457d4c51f13833bb7739b
Girish Lakshmanan <girish.lakshmanan@lexisnexis.co.uk>
2017-07-04 17:51:31 +0100

    added  configureJacksonObjectMapper

--- src/main/java/com/globi/infa/datasource/core/DataRepositoryConfig.java
+++ src/main/java/com/globi/infa/datasource/core/DataRepositoryConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.globi.infa.workflow.InfaWorkflow;
 import com.globi.infa.workflow.PTPWorkflow;
 import com.globi.metadata.sourcesystem.SourceSystem;
@@ -18,5 +20,10 @@ public class DataRepositoryConfig extends RepositoryRestConfigurerAdapter {
         config.exposeIdsFor(InfaWorkflow.class);
         config.exposeIdsFor(PTPWorkflow.class);
         config.exposeIdsFor(SourceSystem.class);
+    }
+    
+    @Override
+    public void configureJacksonObjectMapper(ObjectMapper objectMapper) {
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
     }
 }
