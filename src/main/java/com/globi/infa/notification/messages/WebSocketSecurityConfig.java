de5256c8cd5a8270c1e276fe9ccb3baa9cbe6c1f
Girish Lakshmanan <girish.lakshmanan@lexisnexis.co.uk>
2017-07-11 17:13:55 +0100

    removed websocket security

--- src/main/java/com/globi/infa/notification/messages/WebSocketSecurityConfig.java
+++ src/main/java/com/globi/infa/notification/messages/WebSocketSecurityConfig.java
@@ -1,21 +1,21 @@
-package com.globi.infa.notification.messages;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-
-@Configuration
-public class WebSocketSecurityConfig
-      extends AbstractSecurityWebSocketMessageBrokerConfigurer { 
-
-	 @Override
-	    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-	        messages
-	                .nullDestMatcher().authenticated() 
-	                .simpSubscribeDestMatchers("/workflows").permitAll() 
-	                .simpDestMatchers("/workflows/**").permitAll() 
-	                .simpMessageDestMatchers("/**").permitAll()
-	                .anyMessage().permitAll();
-
-	    }
-}
+//package com.globi.infa.notification.messages;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+//import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//public class WebSocketSecurityConfig
+//      extends AbstractSecurityWebSocketMessageBrokerConfigurer { 
+//
+//	 @Override
+//	    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//	        messages
+//	                .nullDestMatcher().authenticated() 
+//	                .simpSubscribeDestMatchers("/workflows").permitAll() 
+//	                .simpDestMatchers("/workflows/**").permitAll() 
+//	                .simpMessageDestMatchers("/**").permitAll()
+//	                .anyMessage().permitAll();
+//
+//	    }
+//}
